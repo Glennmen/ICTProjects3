5b1582d1ceb55302ce45af2d506cf30398a8c981
Boelen <tom.boelen@student.khlim.be>
2015-10-09 11:36:08 +0200

    AddScoreActivity toegevoegd

---
+++ MobieleAppBowling/app/src/main/java/com/example/ictprojects/mobieleappbowling/AddScoreActivity.java
@@ -0,0 +1,16 @@
+package com.example.ictprojects.mobieleappbowling;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tom on 9/10/2015.
+ */
+public class AddScoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addscore_main);
+    }
+}
