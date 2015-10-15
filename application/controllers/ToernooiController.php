968477eff2cbe8a6717e33e1b2bfc08ee94f9414
Sarah <hansensarah1@hotmail.com>
2015-10-15 17:16:50 +0200

    Enkel namen aangepast aan de view

--- application/controllers/ToernooiController.php
+++ application/controllers/ToernooiController.php
@@ -9,9 +9,9 @@ class ToernooiController extends CI_Controller{
         $this->load->library('navbar');
         $data['nav'] = $this->navbar->get_navbar();
         
-        $this->form_validation->set_rules('ToernooiName', 'Name','required');
-        $this->form_validation->set_rules('ToernooiStartDate', 'StartDate','required');
-        $this->form_validation->set_rules('ToernooiEndDate', 'EndDate','required');
+        $this->form_validation->set_rules('tournamentName', 'Name','required');
+        $this->form_validation->set_rules('tournamentStartDate', 'StartDate','required');
+        $this->form_validation->set_rules('tournamentEndDate', 'EndDate','required');
         $this->form_validation->set_rules('location', 'location','required');
         
         if(isset($_POST['CreateToernooi'])){
@@ -19,9 +19,9 @@ class ToernooiController extends CI_Controller{
                 $this->load->view('ToernooiView', $data);
             }else{
               $aToernooiData = [
-                  'naam' => $this->input->post('ToernooiName'),
-                  'startDatum' => $this->input->post('ToernooiStartDate'),
-                  'eindDatum' => $this->input->post('ToernooiEndDate'),
+                  'naam' => $this->input->post('tournamentName'),
+                  'startDatum' => $this->input->post('tournamentStartDate'),
+                  'eindDatum' => $this->input->post('tournamentEndDate'),
                   'locatie' => $this->input->post('location'),
                   'tournooi_id' => $this->input->post('type') 
               ];
@@ -29,11 +29,11 @@ class ToernooiController extends CI_Controller{
               $this->load->model('Toernooi_model');
               $this->Toernooi_model->ToernooiToevoegen($aToernooiData);
               
-              $this->load->view('ToernooiView', $data);
+              $this->load->view('TournamentView', $data);
             }
         }else{
             
-            $this->load->view('ToernooiView', $data);
+            $this->load->view('TournamentView', $data);
         }
         
     }
