4483e227a6d37017934ad20f3f780141457d894c
robin <robinvdh92@hotmail.com>
2015-10-23 12:45:08 +0200

    Funties toevoegen voor ACCEPTED,DECLINED of geen van bijde

--- application/models/TournamentList_model.php
+++ application/models/TournamentList_model.php
@@ -11,5 +11,29 @@ class TournamentList_model extends CI_Model {
         
        return $aData;
     }
+    public function AllNotAcceptedTournaments($googleID)
+    {
+        $this->load->database();
+        
+        $aData =$this->db->query("SELECT Toernooi_ID, Eigenaar_ID, Begin_Datum, Eind_Datum, Naam FROM tournament
+                                    INNER JOIN deelnemerstournooi ON tournament.Toernooi_ID = deelnemerstournooi.tournooiID WHERE accepted = 0 and googleID=".$googleID);
+        return $aData;
+    }
+    public function AllAcceptedTournaments($googleID)
+    {
+        $this->load->database();
+        
+        $aData =$this->db->query("SELECT Toernooi_ID, Eigenaar_ID, Begin_Datum, Eind_Datum, Naam FROM tournament
+                                    INNER JOIN deelnemerstournooi ON tournament.Toernooi_ID = deelnemerstournooi.tournooiID WHERE accepted = 1 and googleID=".$googleID);
+        return $aData;
+    }
+    public function AllDeclinedTournaments($googleID)
+    {
+        $this->load->database();
+        
+        $aData =$this->db->query("SELECT Toernooi_ID, Eigenaar_ID, Begin_Datum, Eind_Datum, Naam FROM tournament
+                                    INNER JOIN deelnemerstournooi ON tournament.Toernooi_ID = deelnemerstournooi.tournooiID WHERE accepted = 2 and googleID=".$googleID);
+        return $aData;
+    }
 }
 ?>
