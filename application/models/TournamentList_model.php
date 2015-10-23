997db980260703876a740e7c419ef5e8df6a11d4
Sarah <hansensarah1@hotmail.com>
2015-10-23 12:16:57 +0200

    query aangepast

--- application/models/TournamentList_model.php
+++ application/models/TournamentList_model.php
@@ -5,8 +5,8 @@ class TournamentList_model extends CI_Model {
     {
         $this->load->database();
         
-        $aData =$this->db->query("SELECT Toernooi_ID, Eigenaar_ID, Naam FROM tournament
-                                     INNER JOIN deelnemerstournooi ON tournament.Toernooi_ID = deelnemerstournooi.tournooiID WHERE accepted = 1 and googleID=".$googleID);
+        $aData =$this->db->query("SELECT Toernooi_ID, Eigenaar_ID, Begin_Datum, Eind_Datum, Naam FROM tournament
+                                    INNER JOIN deelnemerstournooi ON tournament.Toernooi_ID = deelnemerstournooi.tournooiID WHERE accepted = 1 and googleID=".$googleID);
         return $aData;
     }
 }
