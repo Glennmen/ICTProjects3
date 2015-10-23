de03ba280c0a942242e7f70571fcf125dbcb286f
Trekels <vincent.trekels@gmail.com>
2015-10-23 12:07:02 +0200

    datetime fix

--- application/controllers/GameController.php
+++ application/controllers/GameController.php
@@ -7,13 +7,17 @@ class GameController extends CI_Controller{
     public function index(){
         
         $this->load->library('navbar');
+        
         $data['nav'] = $this->navbar->get_navbar();
+        
+        
         
         $this->form_validation->set_rules('gameName', 'Name','required');
         $this->form_validation->set_rules('gameDate', 'Date','required|callback_dateCheck');
         $this->form_validation->set_rules('gameTime', 'Time','required|callback_timeCheck');
         $this->form_validation->set_rules('location', 'location','required');
        
+        
         
         if(isset($_POST['CreateGame'])){
             if($this->form_validation->run()==FALSE){
@@ -35,9 +39,8 @@ class GameController extends CI_Controller{
         }else{
             
             $this->load->view('gameView1', $data);
-        }
-        
-    }
+        } 
+    } 
     public function dateCheck($strDate){
         
         $format ="d/m/y" ;
@@ -48,9 +51,25 @@ class GameController extends CI_Controller{
         if(checkdate ( $values[1] , $values[0] , $values[2] )){
              return true;
         }else{
-            $this->form_validation->set_message('dateCheck', 'Enter a valid date format');
+            $this->form_validation->set_message('dateCheck', 'Enter a valid date ');
             return FALSE;
         }   
+    }
+    
+    public function timeCheck($strTime){
+        $values = explode(":", $strTime);
+        
+        echo $values[0];
+        echo $values[1];
+        
+        if($values[0] > 23 || $values[1] > 59){
+            $this->form_validation->set_message('timeCheck', 'Enter a valid time ');
+            return FALSE;
+        }else{
+            return true;   
+        }
+        
+         
     }
     
 }
--- application/controllers/TournamentController.php
+++ application/controllers/TournamentController.php
@@ -10,8 +10,8 @@ class TournamentController extends CI_Controller{
         $data['nav'] = $this->navbar->get_navbar();
         
         $this->form_validation->set_rules('tournamentName', 'Name','required');
-        $this->form_validation->set_rules('tournamentStartDate', 'StartDate','required');
-        $this->form_validation->set_rules('tournamentEndDate', 'EndDate','required');
+        $this->form_validation->set_rules('tournamentStartDate', 'StartDate','required|callback_dateCheck');
+        $this->form_validation->set_rules('tournamentEndDate', 'EndDate','required|callback_dateCheck');
         
         if(isset($_POST['CreateTournament'])){
             if($this->form_validation->run()==FALSE){
@@ -34,6 +34,20 @@ class TournamentController extends CI_Controller{
             $this->load->view('TournamentView', $data);
         }
         
+    }
+    public function dateCheck($strDate){
+        
+        $format ="d/m/y" ;
+        $publishDate = DateTime::createFromFormat('d/m/Y', $strDate);
+
+        $values = explode("/", $strDate);
+       
+        if(checkdate ( $values[1] , $values[0] , $values[2] )){
+             return true;
+        }else{
+            $this->form_validation->set_message('dateCheck', 'Enter a valid date ');
+            return FALSE;
+        }   
     }
     
 }
