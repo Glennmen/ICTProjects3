a529180ff8a60b1f58878a9b39e7a614033bc1a7
Boelen <tom.boelen@student.khlim.be>
2015-10-23 12:40:24 +0200

    TourmanentInfo_Model Aangepast

--- application/models/TournamentInfo_model.php
+++ application/models/TournamentInfo_model.php
@@ -12,17 +12,18 @@ class TournamentInfo_model extends CI_Model {
     {
         $this->load->database();
 
-        $aData = $this->db->query("SELECT Totaal,Google_ID,Strikes,Spare FROM score
-                                    INNER JOIN game ON score.GAME_ID = game.GAME_ID WHERE game.Tournooi_ID =".$tournamentID);
+       //$aData = $this->db->query("SELECT Totaal,Google_ID,Strikes,Spare FROM score
+        //                            INNER JOIN game ON score.GAME_ID = game.GAME_ID WHERE game.Tournooi_ID =".$tournamentID);
 
-        $asData = $this->db->query("SELECT Game_ID FROM game");
+        $asData = $this->db->query("SELECT * FROM tournament");
 
         echo '<pre>';
         print_r($asData);
         echo '</pre>';
-        echo "hallo";
 
-        return $aData;
+
+
+        //return $aData;
 
     }
 }
--- application/models/TournamentList_model.php
+++ application/models/TournamentList_model.php
@@ -7,7 +7,9 @@ class TournamentList_model extends CI_Model {
         
         $aData =$this->db->query("SELECT Toernooi_ID, Eigenaar_ID, Begin_Datum, Eind_Datum, Naam FROM tournament
                                     INNER JOIN deelnemerstournooi ON tournament.Toernooi_ID = deelnemerstournooi.tournooiID WHERE accepted = 1 and googleID=".$googleID);
-        return $aData;
+        
+        
+       return $aData;
     }
 }
 ?>
