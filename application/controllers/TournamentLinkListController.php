f9299835c50f6fdc49804386acb49f8592e1c3de
Sarah <hansensarah1@hotmail.com>
2015-10-23 11:59:59 +0200

    TournamentLinkListController en TournamentLinkListView geüpdate, button stuurt data nog niet.

--- application/controllers/TournamentLinkListController.php
+++ application/controllers/TournamentLinkListController.php
@@ -1,8 +1,29 @@
 <?php
+if(!defined('BASEPATH'))exit('No direct acces allowed');
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class TournamentLinkListController extends CI_Controller {
+    public function index(){
+        $this->load->library('navbar');
+        $data['nav'] = $this->navbar->get_navbar();
+        if(isset($_POST['SelectTournament'])){
+            $this->load->view('TournamentInfo');
+            
+        }
+        else
+        {
+            $data['aTournamentListData'] = $this->view_list();
+            $this->load->view('TournamentListView', $data);
+        }
+    }
+    
+    public function view_list(){
+        $this->load->model('TournamentList_model');
+        $result = $this->TournamentList_model->AllTournaments(1); //komt googleID
+        if ($result != false) {
+            return $result;
+        }else{
+            return null;
+        }
+    }
+}
+?>
