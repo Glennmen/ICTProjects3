6c51f8baa4f52c79f302aeb81a2baae065d50c5f
Boelen <tom.boelen@student.khlim.be>
2015-11-20 09:02:50 +0100

    Sessions Update

--- application/controllers/OAuthController.php
+++ application/controllers/OAuthController.php
@@ -9,6 +9,23 @@ class OAuthController extends CI_Controller
         //session_start();
     }
 
+   /* function __construct()
+    {
+        parent::__construct();
+        if ( ! $this->session->userdata('logged_in'))
+        {
+            // Allow some methods?
+            $allowed = array(
+                'some_method_in_this_controller',
+                'other_method_in_this_controller',
+            );
+            if ( ! in_array($this->router->fetch_method(), $allowed)
+            {
+            redirect('login');
+        }
+        }
+    }*/
+
     public function index()
     {
         $sContent = null;
