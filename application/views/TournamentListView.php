94222982273d0b64beff22d5669f8172bb6e464b
Sarah <hansensarah1@hotmail.com>
2015-11-20 09:49:06 +0100

    mooiere lijst

--- application/views/TournamentListView.php
+++ application/views/TournamentListView.php
@@ -8,28 +8,44 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/bootstrap.css"/>
-        <title></title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <title>Tournament List</title>
     </head>
     <body>
     
     <?php echo $nav ?>
     <?php echo validation_errors()?>
         <form action="TournamentListController" method="post">
-            <select name="tournaments" size="5" style="width: 300px">
+        <table>
+            <tr>
+                <th>Select</th>
+                <th>Toernooi naam</th>
+                <th>Begin datum</th>
+                <th>Eind datum</th>
+            </tr>
             <?php 
-                if($aTournamentListData != null){
-                    foreach($aTournamentListData as $aTournamentListRow){
-                        $sContent .= "<option value='".$aTournamentListRow->Google_ID."'>".$aTournamentListRow->Tournament_Name."</option>";
-                    }
-                    echo $sContent;
-                }
-                else{
-                    echo '<select>Geen data beschikbaar</select>';
-                }
-            ?> 
-            </select>
-            <br /><br />
+                 if($aTournamentListData != null){
+                     foreach($aTournamentListData as $aTournamentListRow){
+                        $sContent =  "<tr><td><input name='Selected' type='radio' value='1".$aTournamentListRow->Tournament_ID."'></td>";
+                        $sContent .=  "<td>".$aTournamentListRow->Tournament_Name."</td>";
+                        $sContent .=  "<td>".$aTournamentListRow->Start_Date."</td>";
+                        $sContent .=  "<td>".$aTournamentListRow->End_Date."</td></tr>";
+                        echo $sContent;
+                     }
+                 }
+                 else{
+                    echo '<tr><td colspan="4">Geen data beschikbaar</td></tr>';
+                 }
+                 ?>
+            </table>
             <button type="submit" name="SelectTournament" class="btn btn-default">Selecteer toernooi</button>
         </form>
+        <script>
+            $(function () {
+                $("tr").click(function () {
+                $(this).children().find(":radio").prop('checked', true);
+                })
+            })
+        </script>
     </body>
 </html>
