dec2c32ef4d47c7e53925d4c27d5ea68e674f16e
Boelen <tom.boelen@student.khlim.be>
2015-12-23 13:24:07 +0100

    Require_Login overal aangepast

--- application/controllers/GameController.php
+++ application/controllers/GameController.php
@@ -8,19 +8,21 @@ class GameController extends CI_Controller{
     function __construct()
     {
         parent::__construct();
-        if ( ! $this->session->userdata('logged_in'))
-        {
+        if(empty($_SESSION['Google_ID'])) {
+
+
             // Allow some methods?
             $allowed = array(
-                'MobileApp'
+                'MobileApp',
             );
-             
             if ( ! in_array($this->router->fetch_method(), $allowed))
             {
 
-        }
+                redirect('../OAuthController', 'refresh');
+            }
         }
     }
+
     
     public function index(){
         
