5724841a86f5f1d8857ff5d66cc77a553f1d1425
Glenn Carremans <glennekes@msn.com>
2015-12-23 13:39:43 +0100

    fixed spatie te veel

--- application/controllers/InvitesController.php
+++ application/controllers/InvitesController.php
@@ -13,7 +13,7 @@ class InvitesController extends CI_Controller{
 
             // Allow some methods?
             $allowed = array(
-                ' mobileApp',
+                'mobileApp',
             );
             if (!in_array($this->router->fetch_method(), $allowed)) {
 
