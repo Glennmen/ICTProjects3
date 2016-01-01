7a76705fc33c607beca4deeef20fd818ed27161e
Relicx4 <leandro.cardinaels@gmail.com>
2016-01-01 19:18:54 +0100

    Update TournamentController.php
    Form validation aangepast.

--- application/controllers/TournamentController.php
+++ application/controllers/TournamentController.php
@@ -11,8 +11,8 @@ class TournamentController extends CI_Controller{
         $data['alert'] = "";
         
         $this->form_validation->set_rules('tournamentName', 'Name','required');
-        $this->form_validation->set_rules('tournamentStartDate', 'StartDate','required|callback_dateCheck');
-        $this->form_validation->set_rules('tournamentEndDate', 'EndDate','required|callback_dateCheck');
+        $this->form_validation->set_rules('tournamentStartDate', 'StartDate','required|callback_startDateCheck');
+        $this->form_validation->set_rules('tournamentEndDate', 'EndDate','required|callback_endDateCheck');
         
         if(isset($_POST['CreateTournament'])){
             if($this->form_validation->run()==FALSE){
@@ -20,31 +20,22 @@ class TournamentController extends CI_Controller{
             }else{
               
               $startDate = DateTime::createFromFormat('d/m/Y', $this->input->post('tournamentStartDate'));               
-              $endDate = DateTime::createFromFormat('d/m/Y', $this->input->post('tournamentEndDate'));     
-              $currentDate = date("Y-m-d");
-                
+              $endDate = DateTime::createFromFormat('d/m/Y', $this->input->post('tournamentEndDate'));
               
-              if ((strtotime(date_format($startDate,"Y-m-d")) > strtotime($currentDate)) && (strtotime(date_format($endDate,"Y-m-d")) > strtotime($currentDate)) && (strtotime(date_format($startDate,"Y-m-d")) < strtotime(date_format($endDate,"Y-m-d"))))
-              {
-                $personen = $this->input->post('Personen[]');
+              $personen = $this->input->post('Personen[]');
 
-                $aTournamentData = [
-                    'Tournament_Name' => $this->input->post('tournamentName'),
-                    'Start_Date' => $startDate->format("Y-m-d"),
-                    'End_Date' => $endDate->format("Y-m-d"),
-                    'Google_ID' => $_SESSION['Google_ID'],
-                ];
+              $aTournamentData = [
+                  'Tournament_Name' => $this->input->post('tournamentName'),
+                  'Start_Date' => $startDate->format("Y-m-d"),
+                  'End_Date' => $endDate->format("Y-m-d"),
+                  'Google_ID' => $_SESSION['Google_ID'],
+              ];
 
-                $this->load->model('Tournament_model');
-                $this->Tournament_model->TournamentToevoegen($aTournamentData, $personen);
+              $this->load->model('Tournament_model');
+              $this->Tournament_model->TournamentToevoegen($aTournamentData, $personen);
 
-                $data['alert'] = "<div class='alert alert-success' role='alert'>Toernooi succesvol aangemaakt!</div>";
-                $this->load->view('TournamentView', $data);
-              }
-              else{
-                  $this->form_validation->set_message('dateCheck', 'Enter a valid date ');
-                  return FALSE;
-        }   
+              $data['alert'] = "<div class='alert alert-success' role='alert'>Toernooi succesvol aangemaakt!</div>";
+              $this->load->view('TournamentView', $data);
             }
         }else{
             
@@ -52,16 +43,55 @@ class TournamentController extends CI_Controller{
         }
         
     }
-    public function dateCheck($strDate){
-        
-        $values = explode("/", $strDate);
-       
-        if(checkdate ( $values[1] , $values[0] , $values[2] )){
-             return true;
-        }else{
-            $this->form_validation->set_message('dateCheck', 'Enter a valid date ');
+    
+    public function startDateCheck($strStartDate)
+    {
+         $values = explode("/", $strStartDate);
+         $startDate = DateTime::createFromFormat('d/m/Y', $strStartDate);
+         $endDate = DateTime::createFromFormat('d/m/Y', $this->input->post('tournamentEndDate'));
+         $currentDate = date("Y-m-d");
+         
+         if ((checkdate ( $values[1] , $values[0] , $values[2] )) == FALSE)
+         {
+            $this->form_validation->set_message('startDateCheck', 'Enter a valid start date ');
             return FALSE;
-        }   
+         }
+         elseif (strtotime(date_format($startDate,"Y-m-d")) <= strtotime($currentDate))
+         {
+            $this->form_validation->set_message('startDateCheck', "Tournament start date can't be in the past");
+            return FALSE;
+         }
+         elseif (strtotime(date_format($startDate,"Y-m-d")) > strtotime(date_format($endDate,"Y-m-d")))
+         {
+            $this->form_validation->set_message('startDateCheck', "Tournament start date must be smaller than tournament end date");
+            return FALSE;
+         }
+         else
+         {
+            return TRUE;
+         }
+    }
+    
+    public function endDateCheck($strEndDate)
+    {
+         $values = explode("/", $strEndDate);
+         $endDate = DateTime::createFromFormat('d/m/Y', $strEndDate);
+         $currentDate = date("Y-m-d");
+         
+         if ((checkdate ( $values[1] , $values[0] , $values[2] )) == FALSE)
+         {
+             $this->form_validation->set_message('endDateCheck', 'Enter a valid end date ');
+             return FALSE;
+         }
+         elseif (strtotime(date_format($endDate,"Y-m-d")) <= strtotime($currentDate))
+         {
+            $this->form_validation->set_message('endDateCheck', "Tournament end date can't be in the past");
+            return FALSE;
+         }
+         else
+         {
+            return TRUE;
+         }
     }
     
     public function jsonPersonen() {
